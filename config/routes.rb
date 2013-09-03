07f283e4dadda54c3cd5dfda1b54c16fe02b427b
John Zdanis <Zdanis.john@gmail.com>
2013-09-03 12:29:55 -0400

    changed index and show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Photogur::Application.routes.draw do
   resources :pictures
 
+  root :to => 'pictures#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
