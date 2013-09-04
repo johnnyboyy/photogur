237df6cbe4cca5fae255c32a953b40370cd03e5e
John Zdanis <Zdanis.john@gmail.com>
2013-09-04 11:55:01 -0400

    minor changes, css and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,75 @@ Picture.create!([
     :artist => "John Wilhelm",
     :URL    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"  
   ])
+
+Picture.create!([
+    :title  => "PokePals",
+    :artist => "Unknown",
+    :URL    => "http://i.imgur.com/zvpyENz.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "KitKat",
+    :artist => "Unknown",
+    :URL    => "http://i.imgur.com/uYgNP5B.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Rainbow",
+    :artist => "Unknown",
+    :URL    => "http://i.imgur.com/fhXQB1r.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Sherbert",
+    :artist => "Unknown",
+    :URL    => "http://i.imgur.com/auZcBWL.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Dolphin",
+    :artist => "Unknown",
+    :URL    => "http://i.imgur.com/ZM8GVK7.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Farm",
+    :artist => "Unknown",
+    :URL    => "http://i.imgur.com/24ekOAH.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Batman Kiwi",
+    :artist => "Unknown",
+    :URL    => "http://i.imgur.com/8fm6fBw.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Unknown",
+    :artist => "Simon Stålenhag",
+    :URL    => "http://i.imgur.com/PxggzxA.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Unknown",
+    :artist => "Simon Stålenhag",
+    :URL    => "http://i.imgur.com/8vuFDn5.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Unknown",
+    :artist => "Simon Stålenhag",
+    :URL    => "http://i.imgur.com/7bu1vmc.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Unknown",
+    :artist => "Simon Stålenhag",
+    :URL    => "http://i.imgur.com/YwqhDAB.jpg"  
+  ])
+
+Picture.create!([
+    :title  => "Unknown",
+    :artist => "Simon Stålenhag",
+    :URL    => "http://i.imgur.com/7hmtdJ0.jpg"  
+  ])
